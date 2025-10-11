005edaa91ab2afb76510e28234634e169bed95e2
vjy-07 <gvijaycrl@gmail.com>
2025-10-11 15:52:22 +0530

    backend deployed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SideBar from './component/SideBar/SideBar'
 import {Routes,Route} from 'react-router-dom';
